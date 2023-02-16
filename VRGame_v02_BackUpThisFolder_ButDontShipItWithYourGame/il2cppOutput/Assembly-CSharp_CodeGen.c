3c9419ec4658365e0f806ee2f07c4f0ea6b6fdc2
KID <50666207+KID421@users.noreply.github.com>
2023-02-16 16:33:15 +0800

    Update 備份

---
+++ VRGame_v02_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,317 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void LookCamera::Start()
+extern void LookCamera_Start_mE9289E1E6CB37C5254B8BCAA97DEF20F5B8E3D7B (void);
+// 0x00000002 System.Void LookCamera::Update()
+extern void LookCamera_Update_m68E69B1B5A7045536596C77570DDBB111AC6DEE0 (void);
+// 0x00000003 System.Void LookCamera::.ctor()
+extern void LookCamera__ctor_m14F0C4DDA1FF55558C400791F6361D35EA975CB4 (void);
+// 0x00000004 System.Void SkyboxChanger::Awake()
+extern void SkyboxChanger_Awake_mFA3795D4738C1605BB290573FF271D7F2B22C2F6 (void);
+// 0x00000005 System.Void SkyboxChanger::ChangeSkybox()
+extern void SkyboxChanger_ChangeSkybox_m485924083107A74613F32E18B94E58DB4BDF7A6E (void);
+// 0x00000006 System.Void SkyboxChanger::.ctor()
+extern void SkyboxChanger__ctor_mC961CEC26BEE8B006C7EFF30C777364DBC70884B (void);
+// 0x00000007 System.Void CFX_AutoStopLoopedEffect::OnEnable()
+extern void CFX_AutoStopLoopedEffect_OnEnable_mFAE624B7728A2F5F7865E685F4594BD0C9629FA5 (void);
+// 0x00000008 System.Void CFX_AutoStopLoopedEffect::Update()
+extern void CFX_AutoStopLoopedEffect_Update_m8416521A39549298F203B44EF089B09EBD8793FD (void);
+// 0x00000009 System.Void CFX_AutoStopLoopedEffect::.ctor()
+extern void CFX_AutoStopLoopedEffect__ctor_m66E2F5DD06231BC3D8D01C11A34D7D55BE5F7AAE (void);
+// 0x0000000A System.Void CFX_Demo_New::Awake()
+extern void CFX_Demo_New_Awake_m2BEACA32BC84D821DF07AF25B7B0B99BFE8F7371 (void);
+// 0x0000000B System.Void CFX_Demo_New::Update()
+extern void CFX_Demo_New_Update_m1172B662225E3FC12899D5FEEC66702AEA702EFB (void);
+// 0x0000000C System.Void CFX_Demo_New::OnToggleGround()
+extern void CFX_Demo_New_OnToggleGround_mC1E9AC90083482747297AA60D9CE9ECABF307929 (void);
+// 0x0000000D System.Void CFX_Demo_New::OnToggleCamera()
+extern void CFX_Demo_New_OnToggleCamera_m756EE04AC3DC67A47CFA88C4CC86B1FB9FE0BD02 (void);
+// 0x0000000E System.Void CFX_Demo_New::OnToggleSlowMo()
+extern void CFX_Demo_New_OnToggleSlowMo_m20658A6343C784057EFACEBED012978C4F78D48B (void);
+// 0x0000000F System.Void CFX_Demo_New::OnPreviousEffect()
+extern void CFX_Demo_New_OnPreviousEffect_mA2A209F797976C97EB976A21083FF4C6B9652812 (void);
+// 0x00000010 System.Void CFX_Demo_New::OnNextEffect()
+extern void CFX_Demo_New_OnNextEffect_mBE56FE317B38E35579D4EC0868755EB2F4E4E640 (void);
+// 0x00000011 System.Void CFX_Demo_New::UpdateUI()
+extern void CFX_Demo_New_UpdateUI_m3E3DA5EC770C0F2B84B5206F16CD8A2484BFD6E5 (void);
+// 0x00000012 UnityEngine.GameObject CFX_Demo_New::spawnParticle()
+extern void CFX_Demo_New_spawnParticle_m55467606F2E464D8337565A08442BB4747468A26 (void);
+// 0x00000013 System.Collections.IEnumerator CFX_Demo_New::CheckForDeletedParticles()
+extern void CFX_Demo_New_CheckForDeletedParticles_m9693C5D1E5FB5189A81426EB45A4E1EE6416EE41 (void);
+// 0x00000014 System.Void CFX_Demo_New::prevParticle()
+extern void CFX_Demo_New_prevParticle_m875BF4D9036B6C5B6259FFA0F787F90042B5D5F9 (void);
+// 0x00000015 System.Void CFX_Demo_New::nextParticle()
+extern void CFX_Demo_New_nextParticle_mB5D2F30BF16AC1AD51964810AE12B27744B7E8E1 (void);
+// 0x00000016 System.Void CFX_Demo_New::destroyParticles()
+extern void CFX_Demo_New_destroyParticles_m6E09B94A196E46E199429F9DE2AE0DD0F9079D24 (void);
+// 0x00000017 System.Void CFX_Demo_New::.ctor()
+extern void CFX_Demo_New__ctor_m3F3E772F218F5D0DF6E561E20AE15B042CED6752 (void);
+// 0x00000018 System.Void CFX_Demo_New/<>c::.cctor()
+extern void U3CU3Ec__cctor_mAB1F46E7745FB53F6527920549715240437DAA6A (void);
+// 0x00000019 System.Void CFX_Demo_New/<>c::.ctor()
+extern void U3CU3Ec__ctor_m97F374614077DD1A825F933DED9B82FDCBAF91D4 (void);
+// 0x0000001A System.Int32 CFX_Demo_New/<>c::<Awake>b__16_0(UnityEngine.GameObject,UnityEngine.GameObject)
+extern void U3CU3Ec_U3CAwakeU3Eb__16_0_m269E213B94F91F8696A3055A7EB523172900185C (void);
+// 0x0000001B System.Void CFX_Demo_New/<CheckForDeletedParticles>d__25::.ctor(System.Int32)
+extern void U3CCheckForDeletedParticlesU3Ed__25__ctor_m920C1552D0B704439FAC777FD178B57829E2D1B6 (void);
+// 0x0000001C System.Void CFX_Demo_New/<CheckForDeletedParticles>d__25::System.IDisposable.Dispose()
+extern void U3CCheckForDeletedParticlesU3Ed__25_System_IDisposable_Dispose_m26C778C5C8416F55F436434D8F71289B81D9219E (void);
+// 0x0000001D System.Boolean CFX_Demo_New/<CheckForDeletedParticles>d__25::MoveNext()
+extern void U3CCheckForDeletedParticlesU3Ed__25_MoveNext_mC7D590156EDDFF76ECD8798F8E7FF09A033ECBA0 (void);
+// 0x0000001E System.Object CFX_Demo_New/<CheckForDeletedParticles>d__25::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CCheckForDeletedParticlesU3Ed__25_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mCDF3D48683749224B09CCBFE3DCBE2B8E1DB72D2 (void);
+// 0x0000001F System.Void CFX_Demo_New/<CheckForDeletedParticles>d__25::System.Collections.IEnumerator.Reset()
+extern void U3CCheckForDeletedParticlesU3Ed__25_System_Collections_IEnumerator_Reset_m766243C9C80375A777FE2A13B85C392B56EB0DFD (void);
+// 0x00000020 System.Object CFX_Demo_New/<CheckForDeletedParticles>d__25::System.Collections.IEnumerator.get_Current()
+extern void U3CCheckForDeletedParticlesU3Ed__25_System_Collections_IEnumerator_get_Current_mAF1404FDA33F8CB72BCF184A9F9C2F32F8329ABA (void);
+// 0x00000021 System.Void CFX_Demo_RandomDir::Start()
+extern void CFX_Demo_RandomDir_Start_mBEA6B11F6CDBC5D92B9039AA8B18A7EBEF5448C8 (void);
+// 0x00000022 System.Void CFX_Demo_RandomDir::.ctor()
+extern void CFX_Demo_RandomDir__ctor_m0D93F82C71A0EAE6FF074A8F3EDBBB81ED3D0B48 (void);
+// 0x00000023 System.Void CFX_Demo_RandomDirectionTranslate::Start()
+extern void CFX_Demo_RandomDirectionTranslate_Start_m96E0BCED718092574499B7127C3A8673849208DB (void);
+// 0x00000024 System.Void CFX_Demo_RandomDirectionTranslate::Update()
+extern void CFX_Demo_RandomDirectionTranslate_Update_m4024B24E39F74439D91BB2A5C0077350853814AE (void);
+// 0x00000025 System.Void CFX_Demo_RandomDirectionTranslate::.ctor()
+extern void CFX_Demo_RandomDirectionTranslate__ctor_m0069918B310E5DB0ABB7A4081DAE7EAFD6A1E0CF (void);
+// 0x00000026 System.Void CFX_Demo_RotateCamera::Update()
+extern void CFX_Demo_RotateCamera_Update_mF984EC78638612B1C89597528E3FD5ACBD2539A4 (void);
+// 0x00000027 System.Void CFX_Demo_RotateCamera::.ctor()
+extern void CFX_Demo_RotateCamera__ctor_m0ADA46D335888F3F43AC518ED19FFB1F1AABC7C8 (void);
+// 0x00000028 System.Void CFX_Demo_RotateCamera::.cctor()
+extern void CFX_Demo_RotateCamera__cctor_m4A71CF2705030EA82F2818BD43F82DC91C01486C (void);
+// 0x00000029 System.Void CFX_Demo_Translate::Start()
+extern void CFX_Demo_Translate_Start_m2408E76C85181E0D900CEFEA8E3CE9214763AC76 (void);
+// 0x0000002A System.Void CFX_Demo_Translate::Update()
+extern void CFX_Demo_Translate_Update_mD71C66B91F9EC43DD9ABBA2D8A45CBD57DE8EBF7 (void);
+// 0x0000002B System.Void CFX_Demo_Translate::.ctor()
+extern void CFX_Demo_Translate__ctor_m44AD33D5825BE599180B3ED3E3668893F46E29DB (void);
+// 0x0000002C System.Void CFX_AutoDestructShuriken::OnEnable()
+extern void CFX_AutoDestructShuriken_OnEnable_m95AB27DC91BCCF60D378AE0665B095D44331D78A (void);
+// 0x0000002D System.Collections.IEnumerator CFX_AutoDestructShuriken::CheckIfAlive()
+extern void CFX_AutoDestructShuriken_CheckIfAlive_mCFAEBF84CD35FBF9C742C9A170183136AAB53535 (void);
+// 0x0000002E System.Void CFX_AutoDestructShuriken::.ctor()
+extern void CFX_AutoDestructShuriken__ctor_m22A7ABD6E38836099374390249F88639BB36CC33 (void);
+// 0x0000002F System.Void CFX_AutoDestructShuriken/<CheckIfAlive>d__2::.ctor(System.Int32)
+extern void U3CCheckIfAliveU3Ed__2__ctor_m107B0D89D95623B02336384ED1123B289D20231B (void);
+// 0x00000030 System.Void CFX_AutoDestructShuriken/<CheckIfAlive>d__2::System.IDisposable.Dispose()
+extern void U3CCheckIfAliveU3Ed__2_System_IDisposable_Dispose_m38FD34408CDD7ED7A963CD1156B55F7F313610A1 (void);
+// 0x00000031 System.Boolean CFX_AutoDestructShuriken/<CheckIfAlive>d__2::MoveNext()
+extern void U3CCheckIfAliveU3Ed__2_MoveNext_m18608B4F0601A8AEFEF69DDE44DBD191B198E3AB (void);
+// 0x00000032 System.Object CFX_AutoDestructShuriken/<CheckIfAlive>d__2::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CCheckIfAliveU3Ed__2_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m655B604E1C89420955196F4D9523D3B6CBACFF9B (void);
+// 0x00000033 System.Void CFX_AutoDestructShuriken/<CheckIfAlive>d__2::System.Collections.IEnumerator.Reset()
+extern void U3CCheckIfAliveU3Ed__2_System_Collections_IEnumerator_Reset_m8659443A2B072F0CFA4D0D77AFD3821588EE1068 (void);
+// 0x00000034 System.Object CFX_AutoDestructShuriken/<CheckIfAlive>d__2::System.Collections.IEnumerator.get_Current()
+extern void U3CCheckIfAliveU3Ed__2_System_Collections_IEnumerator_get_Current_mC14FB0B50310D1A3AD07A78314A0F5690B823799 (void);
+// 0x00000035 System.Void CFX_AutoRotate::Update()
+extern void CFX_AutoRotate_Update_m6B9C72C2FC80E6C5FABD9DB647C8FDAE2BD56328 (void);
+// 0x00000036 System.Void CFX_AutoRotate::.ctor()
+extern void CFX_AutoRotate__ctor_m66822106F9299AE6FB3E7BEE628175E86FAEF866 (void);
+// 0x00000037 System.Void CFX_LightFlicker::Awake()
+extern void CFX_LightFlicker_Awake_mDD217C09D0CD73F750E12C784CF9B9671729817A (void);
+// 0x00000038 System.Void CFX_LightFlicker::OnEnable()
+extern void CFX_LightFlicker_OnEnable_mBAD2393E6F61D5D7981870E576F1238783ED8E3C (void);
+// 0x00000039 System.Void CFX_LightFlicker::Update()
+extern void CFX_LightFlicker_Update_m7EDE8F7D320D87C751799FE5EAEECAF24F33315E (void);
+// 0x0000003A System.Void CFX_LightFlicker::.ctor()
+extern void CFX_LightFlicker__ctor_mAB98FAF050C7B8A46472C42EBE3D802CBDFBE96C (void);
+// 0x0000003B System.Void CFX_LightIntensityFade::Start()
+extern void CFX_LightIntensityFade_Start_mA64A524D6D15ADAE266FF2F3F8F81F38ACBD3EB6 (void);
+// 0x0000003C System.Void CFX_LightIntensityFade::OnEnable()
+extern void CFX_LightIntensityFade_OnEnable_m56FA183840193584063C3B0AF49B0FA8DB8D7014 (void);
+// 0x0000003D System.Void CFX_LightIntensityFade::Update()
+extern void CFX_LightIntensityFade_Update_mCE17E56AE5A9CE5993315D8F00B6AD2C9BEC08B4 (void);
+// 0x0000003E System.Void CFX_LightIntensityFade::.ctor()
+extern void CFX_LightIntensityFade__ctor_m0CFBDFFAFF4B1B51471D1B4AE3A3DE92BB8EE722 (void);
+// 0x0000003F System.Void BallManager::OnCollisionEnter(UnityEngine.Collision)
+extern void BallManager_OnCollisionEnter_mD98D55FA864387C047BDB14B21C576D0C3788AAC (void);
+// 0x00000040 System.Void BallManager::.ctor()
+extern void BallManager__ctor_m208049DB73581F1FABD61F99870B451917D0D260 (void);
+// 0x00000041 System.Void SceneSystem::Quit()
+extern void SceneSystem_Quit_m8ACE5617B98DFF0E8D7F703210EC0486CA9EDDFA (void);
+// 0x00000042 System.Void SceneSystem::Replay()
+extern void SceneSystem_Replay_m8F10F3FDF9DD8A8ABB6068020B9687652D86C535 (void);
+// 0x00000043 System.Void SceneSystem::.ctor()
+extern void SceneSystem__ctor_m8B85303901C8D45A6B981B1FB054DE3CD6D83A61 (void);
+// 0x00000044 System.Void SpawnManager::Spawn()
+extern void SpawnManager_Spawn_m7EECD94AA36394B11891AA73D913D74D0ED61F4F (void);
+// 0x00000045 System.Void SpawnManager::.ctor()
+extern void SpawnManager__ctor_m8DD503A0FFE79FA38CF0B7F013E54D24A04D166A (void);
+static Il2CppMethodPointer s_methodPointers[69] = 
+{
+	LookCamera_Start_mE9289E1E6CB37C5254B8BCAA97DEF20F5B8E3D7B,
+	LookCamera_Update_m68E69B1B5A7045536596C77570DDBB111AC6DEE0,
+	LookCamera__ctor_m14F0C4DDA1FF55558C400791F6361D35EA975CB4,
+	SkyboxChanger_Awake_mFA3795D4738C1605BB290573FF271D7F2B22C2F6,
+	SkyboxChanger_ChangeSkybox_m485924083107A74613F32E18B94E58DB4BDF7A6E,
+	SkyboxChanger__ctor_mC961CEC26BEE8B006C7EFF30C777364DBC70884B,
+	CFX_AutoStopLoopedEffect_OnEnable_mFAE624B7728A2F5F7865E685F4594BD0C9629FA5,
+	CFX_AutoStopLoopedEffect_Update_m8416521A39549298F203B44EF089B09EBD8793FD,
+	CFX_AutoStopLoopedEffect__ctor_m66E2F5DD06231BC3D8D01C11A34D7D55BE5F7AAE,
+	CFX_Demo_New_Awake_m2BEACA32BC84D821DF07AF25B7B0B99BFE8F7371,
+	CFX_Demo_New_Update_m1172B662225E3FC12899D5FEEC66702AEA702EFB,
+	CFX_Demo_New_OnToggleGround_mC1E9AC90083482747297AA60D9CE9ECABF307929,
+	CFX_Demo_New_OnToggleCamera_m756EE04AC3DC67A47CFA88C4CC86B1FB9FE0BD02,
+	CFX_Demo_New_OnToggleSlowMo_m20658A6343C784057EFACEBED012978C4F78D48B,
+	CFX_Demo_New_OnPreviousEffect_mA2A209F797976C97EB976A21083FF4C6B9652812,
+	CFX_Demo_New_OnNextEffect_mBE56FE317B38E35579D4EC0868755EB2F4E4E640,
+	CFX_Demo_New_UpdateUI_m3E3DA5EC770C0F2B84B5206F16CD8A2484BFD6E5,
+	CFX_Demo_New_spawnParticle_m55467606F2E464D8337565A08442BB4747468A26,
+	CFX_Demo_New_CheckForDeletedParticles_m9693C5D1E5FB5189A81426EB45A4E1EE6416EE41,
+	CFX_Demo_New_prevParticle_m875BF4D9036B6C5B6259FFA0F787F90042B5D5F9,
+	CFX_Demo_New_nextParticle_mB5D2F30BF16AC1AD51964810AE12B27744B7E8E1,
+	CFX_Demo_New_destroyParticles_m6E09B94A196E46E199429F9DE2AE0DD0F9079D24,
+	CFX_Demo_New__ctor_m3F3E772F218F5D0DF6E561E20AE15B042CED6752,
+	U3CU3Ec__cctor_mAB1F46E7745FB53F6527920549715240437DAA6A,
+	U3CU3Ec__ctor_m97F374614077DD1A825F933DED9B82FDCBAF91D4,
+	U3CU3Ec_U3CAwakeU3Eb__16_0_m269E213B94F91F8696A3055A7EB523172900185C,
+	U3CCheckForDeletedParticlesU3Ed__25__ctor_m920C1552D0B704439FAC777FD178B57829E2D1B6,
+	U3CCheckForDeletedParticlesU3Ed__25_System_IDisposable_Dispose_m26C778C5C8416F55F436434D8F71289B81D9219E,
+	U3CCheckForDeletedParticlesU3Ed__25_MoveNext_mC7D590156EDDFF76ECD8798F8E7FF09A033ECBA0,
+	U3CCheckForDeletedParticlesU3Ed__25_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mCDF3D48683749224B09CCBFE3DCBE2B8E1DB72D2,
+	U3CCheckForDeletedParticlesU3Ed__25_System_Collections_IEnumerator_Reset_m766243C9C80375A777FE2A13B85C392B56EB0DFD,
+	U3CCheckForDeletedParticlesU3Ed__25_System_Collections_IEnumerator_get_Current_mAF1404FDA33F8CB72BCF184A9F9C2F32F8329ABA,
+	CFX_Demo_RandomDir_Start_mBEA6B11F6CDBC5D92B9039AA8B18A7EBEF5448C8,
+	CFX_Demo_RandomDir__ctor_m0D93F82C71A0EAE6FF074A8F3EDBBB81ED3D0B48,
+	CFX_Demo_RandomDirectionTranslate_Start_m96E0BCED718092574499B7127C3A8673849208DB,
+	CFX_Demo_RandomDirectionTranslate_Update_m4024B24E39F74439D91BB2A5C0077350853814AE,
+	CFX_Demo_RandomDirectionTranslate__ctor_m0069918B310E5DB0ABB7A4081DAE7EAFD6A1E0CF,
+	CFX_Demo_RotateCamera_Update_mF984EC78638612B1C89597528E3FD5ACBD2539A4,
+	CFX_Demo_RotateCamera__ctor_m0ADA46D335888F3F43AC518ED19FFB1F1AABC7C8,
+	CFX_Demo_RotateCamera__cctor_m4A71CF2705030EA82F2818BD43F82DC91C01486C,
+	CFX_Demo_Translate_Start_m2408E76C85181E0D900CEFEA8E3CE9214763AC76,
+	CFX_Demo_Translate_Update_mD71C66B91F9EC43DD9ABBA2D8A45CBD57DE8EBF7,
+	CFX_Demo_Translate__ctor_m44AD33D5825BE599180B3ED3E3668893F46E29DB,
+	CFX_AutoDestructShuriken_OnEnable_m95AB27DC91BCCF60D378AE0665B095D44331D78A,
+	CFX_AutoDestructShuriken_CheckIfAlive_mCFAEBF84CD35FBF9C742C9A170183136AAB53535,
+	CFX_AutoDestructShuriken__ctor_m22A7ABD6E38836099374390249F88639BB36CC33,
+	U3CCheckIfAliveU3Ed__2__ctor_m107B0D89D95623B02336384ED1123B289D20231B,
+	U3CCheckIfAliveU3Ed__2_System_IDisposable_Dispose_m38FD34408CDD7ED7A963CD1156B55F7F313610A1,
+	U3CCheckIfAliveU3Ed__2_MoveNext_m18608B4F0601A8AEFEF69DDE44DBD191B198E3AB,
+	U3CCheckIfAliveU3Ed__2_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m655B604E1C89420955196F4D9523D3B6CBACFF9B,
+	U3CCheckIfAliveU3Ed__2_System_Collections_IEnumerator_Reset_m8659443A2B072F0CFA4D0D77AFD3821588EE1068,
+	U3CCheckIfAliveU3Ed__2_System_Collections_IEnumerator_get_Current_mC14FB0B50310D1A3AD07A78314A0F5690B823799,
+	CFX_AutoRotate_Update_m6B9C72C2FC80E6C5FABD9DB647C8FDAE2BD56328,
+	CFX_AutoRotate__ctor_m66822106F9299AE6FB3E7BEE628175E86FAEF866,
+	CFX_LightFlicker_Awake_mDD217C09D0CD73F750E12C784CF9B9671729817A,
+	CFX_LightFlicker_OnEnable_mBAD2393E6F61D5D7981870E576F1238783ED8E3C,
+	CFX_LightFlicker_Update_m7EDE8F7D320D87C751799FE5EAEECAF24F33315E,
+	CFX_LightFlicker__ctor_mAB98FAF050C7B8A46472C42EBE3D802CBDFBE96C,
+	CFX_LightIntensityFade_Start_mA64A524D6D15ADAE266FF2F3F8F81F38ACBD3EB6,
+	CFX_LightIntensityFade_OnEnable_m56FA183840193584063C3B0AF49B0FA8DB8D7014,
+	CFX_LightIntensityFade_Update_mCE17E56AE5A9CE5993315D8F00B6AD2C9BEC08B4,
+	CFX_LightIntensityFade__ctor_m0CFBDFFAFF4B1B51471D1B4AE3A3DE92BB8EE722,
+	BallManager_OnCollisionEnter_mD98D55FA864387C047BDB14B21C576D0C3788AAC,
+	BallManager__ctor_m208049DB73581F1FABD61F99870B451917D0D260,
+	SceneSystem_Quit_m8ACE5617B98DFF0E8D7F703210EC0486CA9EDDFA,
+	SceneSystem_Replay_m8F10F3FDF9DD8A8ABB6068020B9687652D86C535,
+	SceneSystem__ctor_m8B85303901C8D45A6B981B1FB054DE3CD6D83A61,
+	SpawnManager_Spawn_m7EECD94AA36394B11891AA73D913D74D0ED61F4F,
+	SpawnManager__ctor_m8DD503A0FFE79FA38CF0B7F013E54D24A04D166A,
+};
+static const int32_t s_InvokerIndices[69] = 
+{
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+	3396,
+	3396,
+	3488,
+	3488,
+	3488,
+	3488,
+	5314,
+	3488,
+	1097,
+	2830,
+	3488,
+	3335,
+	3396,
+	3488,
+	3396,
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+	5314,
+	3488,
+	3488,
+	3488,
+	3488,
+	3396,
+	3488,
+	2830,
+	3488,
+	3335,
+	3396,
+	3488,
+	3396,
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+	2849,
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+	3488,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	69,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
